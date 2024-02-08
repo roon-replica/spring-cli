6ab40ba20d59c9a9d85ef70789a721cc0633cc13
roon-replica <think-roon@naver.com>
2024-02-08 21:05:08 +0900

    참고: https://docs.spring.io/spring-cli/reference/user-command-guide.html

---
+++ .spring/commands/controller/new/RestController.java
@@ -0,0 +1,10 @@
+// Templated Java File
+package {{root-package}}.{{feature}}
+
+@RestController
+public class {{capitalizeFirst feature}}Controller {
+	@GetMapping("/{{feature}}")
+	public String greeting(){
+		return "Hello {{feature}}";
+	}
+}
